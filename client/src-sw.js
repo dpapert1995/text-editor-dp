5b8375c94019fb622b4b9fa0920b53fa27d03c21
Daniel Papert <dpapert@gmail.com>
2022-12-05 21:37:56 -0500

    Added logic to database for PUT and GET requests.

--- client/src-sw.js
+++ client/src-sw.js
@@ -26,7 +26,6 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
 registerRoute(  ({ request }) => request.mode === 'navigate', pageCache);
 
 // Set up asset cache
--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,37 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+console.log('PUT to the database');
+    // Connects to database
+    const contactDb = await openDB('jate', 1);
+    // Create new transaction
+    const tx = contactDb.transaction('jate', 'readwrite');
+    // Create object to store transaction
+    const store = tx.objectStore('jate');
+    // Request store to put in content
+    const request = store.put({ id: 1, value: content });
+    // Get confirmation of the request.
+    const result = await request;
+    console.log('🚀 - data saved to the database', result);
+  };
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  console.log('GET from the database');
+    // Connects to database
+    const contactDb = await openDB('jate', 1);
+    // Create object to store transaction
+    const tx = contactDb.transaction('jate', 'readonly');
+    // Create object to store transaction
+    const store = tx.objectStore('jate');
+    // Gets all data in database
+    const request = store.getAll();
+    // Get confirmation of the request.
+    const result = await request;
+    console.log('result.value', result);
+    return result?.value;
+  };
 
 initdb();
